c24953e694ca55cce9185cc65276a46dd13360d8
rayanrithish3-hue <rayanrithish3@gmail.com>
2026-07-03 01:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ started=true;
 
 music.play();
 
-heart.style.filter="drop-shadow(0 0 40px #ff8cff)";
+heart.style.filter=
+"drop-shadow(0 0 15px #d7a8ff) drop-shadow(0 0 35px #b14cff) drop-shadow(0 0 55px #8f3dff)";
 
 if(navigator.vibrate){
 
